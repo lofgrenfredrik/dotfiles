87ca0dec42ccc820bc0bb344b6d8a4b103c2b3af
lofgrenfredrik <fr3drik.l@gmail.com>
2015-12-01 17:37:55 +0100

    modified path of files

--- backup.sh
+++ backup.sh
@@ -3,19 +3,19 @@
 # Files to backup
 
 ## Dotfiles
-cp .gitconfig Projects/Repos/dotfiles/
-cp .gitignore_global  Projects/Repos/dotfiles/
-cp .bash_aliases  Projects/Repos/dotfiles/
-cp .bash_profile  Projects/Repos/dotfiles/
+cp ~/.gitconfig ~/Projects/Repos/dotfiles/
+cp ~/.gitignore_global  ~/Projects/Repos/dotfiles/
+cp ~/.bash_aliases  ~/Projects/Repos/dotfiles/
+cp ~/.bash_profile  ~/Projects/Repos/dotfiles/
 
 ## SSH config
-cp .ssh/config Projects/Repos/dotfiles/.ssh/
+cp ~/.ssh/config ~/Projects/Repos/dotfiles/.ssh/
 
 ## oh-my-zsh theme
-cp .oh-my-zsh/themes/ob0do.zsh-theme  Projects/Repos/dotfiles/.oh-my-zsh/themes/
+cp ~/.oh-my-zsh/themes/ob0do.zsh-theme  ~/Projects/Repos/dotfiles/.oh-my-zsh/themes/
 
 ## Backup script
-cp backup.sh  Projects/Repos/dotfiles/
+cp ~/backup.sh  ~/Projects/Repos/dotfiles/
 
 ## Prints message
 printf "\033[1;32m***Backup complete***\033[0m\n"
