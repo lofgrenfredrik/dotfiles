6475bf34c9ff160150159ed2228718ac971cabc6
lofgrenfredrik <fr3drik.l@gmail.com>
2016-07-01 17:55:03 +0200

    new functions for status and diff in repo

--- backup.sh
+++ backup.sh
@@ -1,6 +1,5 @@
 # Files to backup
 
-
 ## Dotfiles
 cp ~/.gitconfig  ~/Projects/Repos/dotfiles/
 cp ~/.gitignore_global  ~/Projects/Repos/dotfiles/
@@ -22,10 +21,13 @@ cp ~/backup.sh  ~/Projects/Repos/dotfiles/
 cp -Rf ~/nanorc  ~/Projects/Repos/dotfiles/
 
 ## Add, commit & push to Github
-cd ~/Projects/Repos/dotfiles
-git add .
-git commit -m "$1"
-git push -u origin master
-
-## Prints message
-printf "\033[1;32m*** Backup complete ***\033[0m\n"
+if [ "$1" ]
+then
+  cd ~/Projects/Repos/dotfiles
+  git add .
+  git commit -m "$1"
+  git push -u origin master
+  printf "\033[1;32m*** Backup and pushed to Github ***\033[0m\n"
+else
+  printf "\033[1;32m*** Backup complete ***\033[0m\n"
+fi
